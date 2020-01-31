0ac688d8b67d4fc0746b9e9b2890b53f1790d15f
OneTransistor <one1transistor@gmail.com>
2020-02-01 00:05:27 +0200

    Update esp8266_https_server_ro.ino

--- esp8266_https_server_ro/esp8266_https_server_ro.ino
+++ esp8266_https_server_ro/esp8266_https_server_ro.ino
@@ -149,7 +149,7 @@ void setup() {
   serverHTTP.on("/", redirectionareHTTPS);
   serverHTTP.begin();
 
-  server.setRSACert(new BearSSL::X509List(serverCert), new BearSSL::PrivateKey(serverKey));
+  server.getServer().setRSACert(new BearSSL::X509List(serverCert), new BearSSL::PrivateKey(serverKey));
   server.on("/", afisareSite);
   server.begin();
   
